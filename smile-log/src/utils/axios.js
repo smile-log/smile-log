1763eba6f5f32d077d0372cdd4d2795eb46a4f58
hualuo0609 <2406323182@qq.com>
2019-04-05 21:33:04 +0800

    提价一些配置文件

---
+++ smile-log/src/utils/axios.js
@@ -0,0 +1,22 @@
+import Axios from 'axios'
+import { Component } from "react";
+
+// Add a request interceptor
+Axios.interceptors.request.use(function (config) {
+   // Do something before request is sent
+   return config;
+ }, function (error) {
+   // Do something with request error
+   return Promise.reject(error);
+ });
+
+// Add a response interceptor
+Axios.interceptors.response.use(function (response) {
+   // Do something with response data
+   return response.data;
+ }, function (error) {
+   // Do something with response error
+   return Promise.reject(error);
+ });
+
+Component.prototype.$axios=Axios
